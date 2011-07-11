4d977447012fcf701a5a9259b423ae5fab03bf52
adrianp <panescu.adrian.noreply@gmail.com>
2011-07-11 15:54:33 +0300

    solved third problem

---
+++ p3.py
@@ -0,0 +1,31 @@
+from math import sqrt
+import sys
+
+def main2(n):
+  factor = 1
+  
+  #while this is not necessary, checking now for division by 2 means we won't have
+  #to check for even factors lately
+  if n % 2 == 0:
+    factor = 2
+    n /= 2
+    while n % 2 == 0:
+      n /= 2
+      
+  current = 3
+  maxval = sqrt(n)
+  while n>1 and current<=maxval:
+    if n % current == 0:
+      factor = current
+      n /= current
+      while n % current == 0:
+        n /= current
+      maxval = sqrt(n)
+    current += 2 #we already checked for even factors
+  if n == 1:
+    print factor
+  else:
+    print n #is a prime itself
+
+if __name__ == "__main__":
+  sys.exit(main2(600851475143))
