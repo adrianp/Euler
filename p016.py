df064a781eff3ef3109220d2122a1b9045209f71
Adrian-Tudor Panescu <tudor@figshare.com>
2022-11-01 16:19:58 +0200

    Fix problem 16

--- p016.py
+++ p016.py
@@ -4,9 +4,10 @@ import sys
 def main():
     n, s = 2 ** 1000, 0
     while n:
-        s, n = s + n % 10, n / 10
-    print s
+        s, n = s + n % 10, n // 10
+    print(s)
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
