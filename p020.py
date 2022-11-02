34be5a732590b6eba77e698ea93590215f082936
Adrian-Tudor Panescu <tudor@figshare.com>
2022-11-02 15:53:26 +0200

    Fix problem 20

--- p020.py
+++ p020.py
@@ -1,13 +1,15 @@
-from math import factorial
 import sys
+
+from math import factorial
 
 
 def main():
     n, s = factorial(100), 0
     while n:
-        s, n = s + n % 10, n / 10
-    print s
+        s, n = s + n % 10, n // 10
+    print(s)
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
