8dda0fb6343e2c7b9da167087554e16596696de2
Adrian-Tudor Panescu <tudor@figshare.com>
2022-11-02 15:59:50 +0200

    Fix problem 25

--- p025.py
+++ p025.py
@@ -7,8 +7,9 @@ def main():
     i = 0
     while len(str(fibonacci(i))) < 1000:
         i += 1
-    print i - 1
+    print(i - 1)
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
