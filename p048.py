3751ea645b89e31d74f3c80f733e6ca891a5a62d
Adrian-Tudor Panescu <tudor@figshare.com>
2022-11-02 16:19:34 +0200

    Fix problem 48

--- p048.py
+++ p048.py
@@ -5,8 +5,9 @@ def main():
     s = 0
     for i in range(1, 1001):
         s += i ** i
-    print str(s)[-10:]
+    print(str(s)[-10:])
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
